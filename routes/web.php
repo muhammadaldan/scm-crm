0a606523aa45a226097cdb28cf4eaa2e047b5320
Muhammad Aldan <aldan@nurzen.group>
2024-05-29 21:12:57 +0700

    init project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+require __DIR__ . '/auth.php';
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
